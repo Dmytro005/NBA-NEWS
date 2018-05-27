59ecd6984ad902d6bec5dea6af6f69496a29760d
Dmytro005 <harazdovskiy@gmail.com>
2018-05-27 20:51:25 +0300

    Added prettier and eject webpack config

--- src/HOC/Layout/layout.js
+++ src/HOC/Layout/layout.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './layout.css';
+import Header from 'components/Header/header';
 
 class Layout extends Component {
 	render() {
-		console.log(this.props.children);
-
 		return (
 			<div>
-				<p>Header</p>
+				<Header />
 				{this.props.children}
 				<p>Footer</p>
 			</div>
---
+++ src/components/Header/header.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+	return <header>Header whatever</header>;
+};
+
+export default Header;
