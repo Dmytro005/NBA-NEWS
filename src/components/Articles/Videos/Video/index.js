4750afdc238ba235f2881e8fac2b600470daf094
Dmytro005 <harazdovskiy@gmail.com>
2018-06-23 14:31:45 +0300

    Added videos header

--- src/components/Articles/Videos/Video/index.js
+++ src/components/Articles/Videos/Video/index.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
-// import axios from 'utils/axios';
+import Header from './header';
 
-// import styles from '../../articles.css';
-
+import axios from 'utils/axios';
 class VideoArticle extends Component {
+	state = {
+		article: {},
+		team: null
+	};
+	async componentWillMount() {
+		await axios
+			.get(`/videos?id=${this.props.match.params.id}`)
+			.then(article => {
+				axios.get(`teams?id=${article.data[0].team}`).then(team => {
+					this.setState({
+						article: article.data[0],
+						team: team.data[0]
+					});
+				});
+			});
+	}
+
 	render() {
-		return <p>VideoArticle</p>;
+		const { team, article } = this.state;
+		return (
+			<div>
+				<Header team={team} />
+			</div>
+		);
 	}
 }
 
