f5e2015ae0d66e29c15b35ed98f87c439f9d3697
Dmytro Harazdovskiy <harazdovskiy@gmail.com>
2018-09-10 01:03:14 +0300

    Added /sign-in route with form example and styles

--- src/components/SignIn/SignInView.js
+++ src/components/SignIn/SignInView.js
@@ -1,10 +1,83 @@
 import React, { Component } from 'react';
 
-import './SignIn.css';
+import styles from './SignIn.css';
+import FormField from 'components/FormField';
 
 class SignIn extends Component {
+	state = {
+		registerError: null,
+		oading: false,
+		formData: {
+			email: {
+				element: 'input',
+				value: '',
+				config: {
+					name: 'email_input',
+					type: 'email',
+					placeholder: 'Enter your email'
+				},
+				validation: {
+					required: true,
+					email: true
+				},
+				valid: false,
+				touched: false,
+				validationMessage: ''
+			},
+			password: {
+				element: 'input',
+				value: '',
+				config: {
+					name: 'passowrd_input',
+					type: 'passowrd',
+					placeholder: 'Enter your passowrd'
+				},
+				validation: {
+					required: true,
+					passowrd: true
+				},
+				valid: false,
+				touched: false,
+				validationMessage: ''
+			}
+		}
+	};
+
+	updateForm(element) {
+		const newFormData = {
+			...this.state.formData
+		};
+
+		const newElement = {
+			...newFormData[element.id]
+		};
+
+		newElement.value = element.event.target.value;
+		newFormData[element.id] = newElement;
+
+		this.setState({
+			formData: newFormData
+		});
+	}
+
 	render() {
-		return <p>SignIn</p>;
+		return (
+			<div className={styles.logContainer}>
+				<form>
+					<h2>Register / Log in</h2>
+					<FormField
+						id={'email'}
+						formData={this.state.formData.email}
+						change={element => this.updateForm(element)}
+					/>
+					<FormField
+						id={'password'}
+						formData={this.state.formData.password}
+						change={element => this.updateForm(element)}
+					/>
+				</form>
+			</div>
+		);
 	}
 }
 
